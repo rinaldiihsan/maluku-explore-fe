045cfb50a7abffb9b8c4a1c3e2a2eb59858a3e2d
rinaldiihsan <rinaldi.210170168@mhs.unimal.ac.id>
2023-11-29 01:14:54 +0700

    Page:Home-Section Popular Places

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from '../../components/Home/Hero';
+import Section1 from '../../components/Home/Section1';
 
 const Home = () => {
   return (
     <section className="font-poppins">
       <Hero />
+      <Section1 />
     </section>
   );
 };
